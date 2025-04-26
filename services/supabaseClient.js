c78b63fb5f2b87055dde109c366f9d6a89e72fdf
Pawan Dasila <pawandasila06@gmail.com>
2025-04-26 20:49:44 +0530

    issue resolved

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -1,14 +1,20 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANOTATION_URL; // Using the existing env variable name
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const supabase = createClient(
     supabaseUrl, supabaseAnonKey, {
         auth: {
             autoRefreshToken: true,
             persistSession: true,
-            detectSessionInUrl: true
+            detectSessionInUrl: true,
+            cookieOptions: {
+                secure: isProduction,
+                sameSite: 'lax',
+            }
         }
     }
-)
+);
