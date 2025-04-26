2f01a4bd80f8190bc5537dcd98177d3f06183f4b
Pawan Dasila <pawandasila06@gmail.com>
2025-04-26 20:37:05 +0530

    fixed changes

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -24,6 +24,7 @@ const LoginPage = () => {
         alert("Failed to login with Google: " + error.message);
       } else {
         console.log("Login initiated successfully");
+        window.location.href = "/dashboard"
       }
     } catch (err) {
       console.error("Unexpected error during login:", err);
